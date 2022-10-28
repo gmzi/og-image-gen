563ee79d7e8d1b74429ce9369f590d5fe3e76906
gmzi <gmzieres@gmail.com>
2022-10-28 17:48:04 -0400

    add route for blog

---
+++ pages/api/og-blog-reflexion.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @next/next/no-server-import-in-page */
+import { ImageResponse } from '@vercel/og';
+import { NextRequest } from 'next/server';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    // ?title=<title>
+    const hasTitle = searchParams.has('title');
+    const title = hasTitle
+      ? searchParams.get('title')?.slice(0, 100)
+      : 'My default title';
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: 'white',
+            backgroundSize: '150px 150px',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              justifyItems: 'center',
+              fontSize: 40,
+              border: '1px solid black',
+              padding: '.6em',
+              borderRadius: 5,
+            }}
+          >
+            {/* THIS IS THE TEXT: */}
+            R&#x18E;M
+          </div>
+          <div
+            style={{
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              color: 'black',
+              marginTop: 30,
+              padding: '0 120px',
+              lineHeight: 1.4,
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {title}
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
