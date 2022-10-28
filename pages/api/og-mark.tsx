67d741d71c1462b823ad9e0376f6d247db7cb66c
gmzi <gmzieres@gmail.com>
2022-10-28 18:12:39 -0400

    add og image route for mark app

---
+++ pages/api/og-mark.tsx
@@ -0,0 +1,58 @@
+import { ImageResponse } from '@vercel/og';
+import { NextRequest } from 'next/server';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default function handler(req: NextRequest) {
+  try {
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: 'white',
+            backgroundSize: '150px 150px',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              justifyItems: 'center',
+              border: '1px solid black',
+              padding: '.6em',
+              borderRadius: 5,
+            }}
+          >
+            {/* THIS IS THE TEXT: */}
+            <span
+            style={{fontSize: 60}}>|mark|</span>
+            <span
+            style={{fontSize: 40}}
+            >fund comparison tool</span>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
